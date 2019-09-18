fcd99d0b5023f08b639fdd213ef76dd07161d603
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-18 01:15:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join (" and ")
+  elsif array.length == 3
+    array[-1].insert(0,"and ")
+    array.join(", ")
+  else
+    array[-1].insert(0,"and ")
+    array.join(", ")
+  end
 end
+
+
+#returns a string without any additional fomatting when given a 1-element array (FAILED - 1)
+#adds 'and' between elements when given a 2-element array (FAILED - 2)
+#adds commas plus a final 'and' when given a 3-element array (FAILED - 3)
+#correctly formats arrays of lengths greater than three (FAILED - 4)
